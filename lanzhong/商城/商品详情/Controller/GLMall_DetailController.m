e98eff2d4a01323062c1f84f71f1cf1794f84108
龚磊 <1029235277@qq.com>
2017-09-26 15:03:21 +0800

    no message

--- lanzhong/商城/商品详情/Controller/GLMall_DetailController.m
+++ lanzhong/商城/商品详情/Controller/GLMall_DetailController.m
@@ -7,8 +7,13 @@
 //
 
 #import "GLMall_DetailController.h"
+#import "GLMall_DetailSpecCell.h"
 
-@interface GLMall_DetailController ()
+@interface GLMall_DetailController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIView *headerView;//透视图
+@property (weak, nonatomic) IBOutlet UIView *navView;//导航栏View
 
 @end
 
@@ -16,7 +21,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"GLMall_DetailSpecCell" bundle:nil] forCellReuseIdentifier:@"GLMall_DetailSpecCell"];
+    
 }
 
+#pragma mark - UITableViewDelegate UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 3;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    GLMall_DetailSpecCell *cell = [tableView dequeueReusableCellWithIdentifier:@"GLMall_DetailSpecCell"];
+    cell.selectionStyle = 0;
+    
+    return cell;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 180;
+}
 @end
